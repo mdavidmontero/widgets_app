d5b915b0efb2c5b2411fe99be373fa3f8409432b
Mateo Rodriguez <mdavidmontero6@gmail.com>
2024-12-20 21:15:51 -0500

    widgets_finales

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -31,6 +31,47 @@ const appMenuItems = <MenuItem>[
     link: '/cards', 
     icon: Icons.credit_card
   ),
+   MenuItem(
+    title: 'Progress Indicators', 
+    subTitle: 'Generales y controlados', 
+    link: '/progress', 
+    icon: Icons.refresh_rounded
+  ),
+
+  MenuItem(
+    title: 'Snackbars y diálogos', 
+    subTitle: 'Indicadores en pantalla', 
+    link: '/snackbars', 
+    icon: Icons.info_outline
+  ),
+
+  MenuItem(
+    title: 'Animated container', 
+    subTitle: 'Stateful widget animado', 
+    link: '/animated', 
+    icon: Icons.check_box_outline_blank_rounded
+  ),
+
+  MenuItem(
+    title: 'UI Controls + Tiles', 
+    subTitle: 'Una serie de controles de Flutter', 
+    link: '/ui-controls', 
+    icon: Icons.car_rental_outlined
+  ),
+
+  MenuItem(
+    title: 'Introducción a la aplicación', 
+    subTitle: 'Pequeño tutorial introductorio', 
+    link: '/tutorial', 
+    icon: Icons.accessible_rounded
+  ),
+
+  MenuItem(
+    title: 'InfiniteScroll y Pull', 
+    subTitle: 'Listas infinitas y pull to refresh', 
+    link: '/infinite', 
+    icon: Icons.list_alt_rounded
+  ),
 
 
 ];
--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,7 +1,13 @@
 import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+import 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 import 'package:widgets_app/presentation/screens/buttons/button_screen.dart';
 import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
+import 'package:widgets_app/presentation/screens/ininite_scroll/infinite_scroll_screen.dart';
+import 'package:widgets_app/presentation/screens/progress/progress_screen.dart';
+import 'package:widgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+import 'package:widgets_app/presentation/screens/ui/controls_ui_controls_screen.dart';
 
 final appRouter = GoRouter(
   routes: [
@@ -19,6 +25,41 @@ final appRouter = GoRouter(
       path: '/cards',
       name: CardsScreen.name,
       builder: (context, state) => const CardsScreen(),
+    ),
+    GoRoute(
+      path: '/progress',
+      name: ProgressScreen.name,
+      builder: (context, state) => const ProgressScreen(),
+    ),
+
+    GoRoute(
+      path: '/snackbars',
+      name: SnackbarScreen.name,
+      builder: (context, state) => const SnackbarScreen(),
+    ),
+
+    GoRoute(
+      path: '/animated',
+      name: AnimatedScreen.name,
+      builder: (context, state) => const AnimatedScreen(),
+    ),
+
+    GoRoute(
+      path: '/ui-controls',
+      name: UiControlsScreen.name,
+      builder: (context, state) => const UiControlsScreen(),
+    ),
+
+    GoRoute(
+      path: '/tutorial',
+      name: AppTutorialScreen.name,
+      builder: (context, state) => const AppTutorialScreen(),
+    ),
+
+    GoRoute(
+      path: '/infinite',
+      name: InfiniteScrollScreen.name,
+      builder: (context, state) => const InfiniteScrollScreen(),
     ),
   ],
 );
